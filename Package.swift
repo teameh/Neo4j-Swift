db50e55d121ef58c9f893f9dbeeecc8dfa9fee2c
Niklas Saers <niklas@saers.com>
2019-07-02 07:37:57 +0200

    Version 5

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@ import PackageDescription
 let package = Package(
     name: "Theo",
 	platforms: [
-	        .macOS(.v10_14), 
-	                .iOS(.v12), 
-	                .tvOS(.v12),
+	        .macOS(.v10_14),
+	        .iOS(.v12),
+	        .tvOS(.v12)
 	    ],
     products: [
-        .library(name: "Theo", targets: ["Theo"]),
+        .library(name: "Theo", targets: ["Theo"])
     ],
     dependencies: [
-        .package(url: "https://github.com/Neo4j-Swift/Bolt-swift.git", .branch("master")),
+        .package(url: "https://github.com/Neo4j-Swift/Bolt-swift.git", .branch("master"))
         /*.package(url: "https://github.com/lukaskubanek/LoremSwiftum.git", .revision("6c6018aec1c61a40c2aa06a68eb843ce42bca262")),*/
     ],
     targets: [
@@ -21,6 +21,6 @@ let package = Package(
             dependencies: ["Bolt"]),
         .testTarget(
             name: "TheoTests",
-            dependencies: ["Theo", /* "LoremSwiftum" */]),
+            dependencies: ["Theo" /* "LoremSwiftum" */])
     ]
 )
--- Theo.podspec
+++ Theo.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name         = "Theo"
-  s.version      = "5.0.0b1"
+  s.version      = "5.0.0"
   s.summary      = "Open Source Neo4j library for iOS"
 
   s.description  = <<-DESC
@@ -19,8 +19,7 @@ DESC
   s.authors             = { "Niklas Saers" => "niklas@saers.com",
                             "Cory Wiles" => "corywiles@icloud.com" }
 
-  s.source       = { :git => "https://github.com/Neo4j-Swift/Neo4j-Swift.git", :commit => "34930332aa778a71dc5c9f35ac58d5c9043daedd" }
-  #:tag => "#{s.version}" 
+  s.source       = { :git => "https://github.com/Neo4j-Swift/Neo4j-Swift.git", :tag => "#{s.version}" }
 
   s.source_files  = "Classes", "Sources/**/*.swift"
   
